c56ad790cdccb55d2bed677dc18570f60d116d40
Sougoumay <elidjaihamid]gmail.com>
2024-11-30 09:20:13 +0100

    l'utilisation de applicationin insight dans le fichier public/index.html

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
-import { createBrowserHistory } from "history";
-import { ApplicationInsights } from '@microsoft/applicationinsights-web';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-
-const browserHistory = createBrowserHistory({});
-var reactPlugin = new ReactPlugin();
-// *** Add the Click Analytics plug-in. ***
-/* var clickPluginInstance = new ClickAnalyticsPlugin();
-   var clickPluginConfig = {
-     autoCapture: true
-}; */
-var appInsights = new ApplicationInsights({
-    config: {
-        connectionString: 'InstrumentationKey=ebab4efd-6aca-46a2-b692-57dc408ccca3;IngestionEndpoint=https://francecentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://francecentral.livediagnostics.monitor.azure.com/;ApplicationId=a2ce0ebc-7426-4efb-b3e0-2d19111fc6d1',
-        enableAutoRouteTracking: true,
-        // *** If you're adding the Click Analytics plug-in, delete the next line. ***
-        extensions: [reactPlugin],
-        // *** Add the Click Analytics plug-in. ***
-        // extensions: [reactPlugin, clickPluginInstance],
-        extensionConfig: {
-            [reactPlugin.identifier]: { history: browserHistory }
-            // *** Add the Click Analytics plug-in. ***
-            // [clickPluginInstance.identifier]: clickPluginConfig
-        }
-    }
-});
-appInsights.loadAppInsights();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
