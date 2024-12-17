eb18f8b11a6ada8bfaa366d976feb03f82e82ee4
Sougoumay <elidjaihamid]gmail.com>
2024-12-17 21:47:33 +0100

    utilisation de l'url de l'api en tant que variable d'environnement

--- src/services/MemberService.ts
+++ src/services/MemberService.ts
@@ -6,12 +6,11 @@ export interface Member {
     age: number;
 }
 
-//const API_URL = 'https://backend-web-app-avhvardaaag0ffc0.francecentral-01.azurewebsites.net/api/members?type=teacher';
-const API_URL = 'http://localhost:9090';
-
+const API_URL = process.env.REACT_APP_API_URL;
 export const getMembers = async (query: string = ''): Promise<Member[]> => {
     try {
-        const response = await fetch(`${API_URL}/api/members?type=${query}`);
+        const url = query ? `${API_URL}/api/members?type=${query}` : `${API_URL}/api/members`;
+        const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Failed to fetch members');
         }
