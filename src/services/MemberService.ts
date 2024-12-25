d3aff464846ca4dd21ae0f954d6e6b9224fd8e51
Sougoumay <elidjaihamid]gmail.com>
2024-12-25 16:41:48 +0100

    update .gitignore et MemberService.ts

--- src/services/MemberService.ts
+++ src/services/MemberService.ts
@@ -10,6 +10,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 export const getMembers = async (query: string = ''): Promise<Member[]> => {
     try {
         const url = query ? `${API_URL}/api/members?type=${query}` : `${API_URL}/api/members`;
+        console.log(url);
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Failed to fetch members');
