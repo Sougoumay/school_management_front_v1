196c37908d8874651f3f43ee2314395991f2ed4d
Sougoumay <elidjaihamid]gmail.com>
2025-01-03 21:36:07 +0100

    deploy

--- src/services/MemberService.ts
+++ src/services/MemberService.ts
@@ -1,4 +1,3 @@
-// src/services/MemberService.ts
 export interface Member {
     lastName: string;
     firstName: string;
@@ -9,15 +8,7 @@ export interface Member {
 const API_URL = process.env.REACT_APP_API_URL;
 export const getMembers = async (query: string = ''): Promise<Member[]> => {
     try {
-        // console.log("before if");
-        // console.log(API_URL);
-        // if(API_URL === undefined) {
-        //     API_URL = "https://my-app-webapp-1c897190a3dc.azurewebsites.net";
-        // }
-        // console.log("after if");
-        // console.log(API_URL);
         const url = query ? `${API_URL}/api/members?type=${query}` : `${API_URL}/api/members`;
-        console.log(url);
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Failed to fetch members');
